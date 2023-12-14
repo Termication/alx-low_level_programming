84a9429b093ea9138a9b34cc797cb2be9dbd462e
Terminal <terminalkarabo@gmail.com>
2023-12-14 08:49:09 -0800

    task 4 C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this is the main put function
- * Return: 0
-*/
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-                return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
