714535d88724902f37685659e3a2bed908e73fce
Terminal <terminalkarabo@gmail.com>
2023-12-14 14:43:32 -0800

    task 5 friday C program that prints exactly with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
