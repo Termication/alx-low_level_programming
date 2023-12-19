5146fa46a909a8f273be3de723abe14bfc99bf73
Terminal <terminalkarabo@gmail.com>
2023-12-19 06:15:01 -0800

    a function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,18 +4,13 @@
  * main - function that prints the alphabet, in lowercase
  * Return: 0
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
