d0d03fa77e2c017fc0046b6794db3463f77b86e3
Terminal <terminalkarabo@gmail.com>
2023-12-19 06:30:10 -0800

    a function that prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - this function
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+		while (i <= 9)
+		{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
