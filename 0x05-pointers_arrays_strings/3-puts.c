eea268d91eb0b9522499f1fddf55006cd39d1d4e
Terminal <terminalkarabo@gmail.com>
2024-01-02 07:14:52 -0800

    a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - function name
+ * @str: function parameter
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar (str[i]);
+	_putchar('\n');
+}
