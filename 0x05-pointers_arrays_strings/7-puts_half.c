6e16403837fb2d2c21dbc3a44b0f332924979150
Terminal <terminalkarabo@gmail.com>
2024-01-02 08:49:05 -0800

    a function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - function name
+ * @str: function parameter
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+
+		n = (count - 1) / 2;
+	}
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+}
