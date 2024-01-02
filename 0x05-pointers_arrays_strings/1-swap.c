c3953eb9f9af0768145f1e025d5eef3d4be916ab
Terminal <terminalkarabo@gmail.com>
2024-01-02 06:32:05 -0800

    a function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function name
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
