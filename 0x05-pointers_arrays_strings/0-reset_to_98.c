e54a58044e511011a84db4dc5f106f766275f3c1
Terminal <terminalkarabo@gmail.com>
2024-01-02 06:07:33 -0800

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - the function name
+ * @n: function parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this readme for Pointers arrays and strings
