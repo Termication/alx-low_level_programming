68da68b919069663bee397d4513b2a69d0561405
Terminal <terminalkarabo@gmail.com>
2024-01-03 05:00:13 -0800

    a function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * rev_string - function name
  * @s: function parameter
@@ -10,13 +11,14 @@ void rev_string(char *s)
 	int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-		count++;
+	count++;
+
 	for (i = 0; i < count / 2; i++)
 	{
-		char j;
+	char j;
 
-		j = s[i];
-		s[i] = s[count - i];
-		s[count - 1 - i] = j;
+	j = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = j;
 	}
 }
