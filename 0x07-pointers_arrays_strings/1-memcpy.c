2de8c456def0028b3aeaf71c64487930d23b9586
Terminal <terminalkarabo@gmail.com>
2024-01-08 14:20:21 -0800

    a function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _memcpy - main function prototype
+  *
+  * @src: Source parameter
+  *
+  * @dest: Destination parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: Copied memory
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+	dest[k] = src[k];
+	}
+	return (dest);
+}
