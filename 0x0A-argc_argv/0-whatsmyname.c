0bb763b55614cf9e2fb10dda7a917ecf3a6a20de
Terminal <terminalkarabo@gmail.com>
2024-01-12 02:14:28 -0800

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the name
+ * @argc: argument one
+ * @argv: argument two
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
