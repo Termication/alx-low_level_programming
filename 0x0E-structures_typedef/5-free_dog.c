622a7e468dbcdfc12b898ff4e8ed59d36f9b6eb8
Terminal <terminalkarabo@gmail.com>
2024-01-23 08:12:10 -0800

    a function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,13 +6,13 @@
  * free_dog - frees memory allocated for a struct dog
  * @d: struct dog to free
  */
-	void free_dog(dog_t *d)
+void free_dog(dog_t *d)
+{
+	if (d)
 	{
-		if (d)
-		{
-			free(d->name);
-			free(d->owner);
-			free(d);
-		}
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
+}
 
