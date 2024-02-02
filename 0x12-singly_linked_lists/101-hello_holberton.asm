db5ef89d47b92a7bac15d17b27a0018538aa1aeb
Terminal <terminalkarabo@gmail.com>
2024-02-01 19:00:34 -0800

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+global main
+extern printf
+
+section .data
+    format db "Hello, Holberton", 0
+
+section .text
+main:
+    mov   edi, format
+    xor   eax, eax
+    call  printf
+    mov   eax, 0
+    ret
